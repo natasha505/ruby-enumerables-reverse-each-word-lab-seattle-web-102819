64de8affb71030334b987d90ba03b81ced21bf20
Natali kruk <natasha_505@yahoo.com>
2019-10-23 21:27:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(string)
-  array_of_words = ["Hello there, and how are you?"]
-  new = []
-  new << array_of_words.each{ |word word.reverse}
-new
-
+  string.split.collect {|w| w.reverse}.join (" ")
 end
